3783b8f59077a0a34542857ac8a1e969175648c6
Alain <alain@kinomap.net>
2021-05-18 18:20:51 +0200

    Correciton

--- src/question1/Calculator.java
+++ src/question1/Calculator.java
@@ -1,5 +1,15 @@
 package question1;
 
 public class Calculator {
+    private int a;
+    private int b;
 
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int returnResult(Operation op) {
+        return op.calcul(this.a, this.b);
+    }
 }
--- src/question1/Divide.java
+++ src/question1/Divide.java
@@ -1,5 +1,9 @@
 package question1;
 
-public class Divide {
+public class Divide implements Operation {
 
+    @Override
+    public int calcul(int a, int b) {
+        return a / b;
+    }
 }
